391e2c4c093b1e586928a2af20ef83db74c1afae
liulixiang1988 <liulixiang1988@gmail.com>
2016-05-18 23:09:43 +0800

    spark web: request

--- course-ideas/src/main/java/io/github/liulixiang/course/Main.java
+++ course-ideas/src/main/java/io/github/liulixiang/course/Main.java
@@ -3,12 +3,21 @@ package io.github.liulixiang.course;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static spark.Spark.get;
+import static spark.Spark.post;
 import static spark.route.RouteOverview.enableRouteOverview;
 
 public class Main {
     public static void main(String[] args) {
         get("/", (req, res) -> new ModelAndView(null, "index.hbs"), new HandlebarsTemplateEngine());
+        post("/sign-in", (req, res) -> {
+            Map<String, String> model = new HashMap<>();
+            model.put("username", req.queryParams("username"));
+            return new ModelAndView(model, "sign-in.hbs");
+        }, new HandlebarsTemplateEngine());
         enableRouteOverview();
     }
 }
