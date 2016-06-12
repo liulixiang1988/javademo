7c0e009f96dfacc11c6790716c5deecbe714dc87
liulixiang1988 <liulixiang1988@gmail.com>
2016-06-13 07:10:31 +0800

    sql

--- seckill/src/main/sql/schema.sql
+++ seckill/src/main/sql/schema.sql
@@ -10,8 +10,8 @@ CREATE TABLE seckill(
 `name` varchar(120) NOT NULL COMMENT '商品名称',
 `number` int NOT NULL COMMENT '库存数量',
 `start_time` TIMESTAMP NOT NULL COMMENT '秒杀开启时间',
-`end_time` TIMESTAMP NOT NULL COMMENT '秒杀结束时间',
-`create_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT='创建时间',
+`end_time` TIMESTAMP NULL COMMENT '秒杀结束时间',
+`create_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   PRIMARY KEY (seckill_id),
   KEY idx_start_time(start_time),
   KEY idx_end_time(end_time),
@@ -26,3 +26,14 @@ VALUES
 ('500元秒杀ipad2', 250, '2016-6-12 00:00:00', '2016-6-13 00:00:00'),
 ('300元秒杀小米6', 200, '2016-6-12 00:00:00', '2016-6-13 00:00:00'),
 ('100元秒杀红米note', 500, '2016-6-12 00:00:00', '2016-6-13 00:00:00');
+
+
+-- 秒杀成功明细表
+-- 用户登录认证相关信息
+CREATE TABLE success_killed(
+  `seckill_id` BIGINT NOT NULL COMMENT '商品库存id',
+  `user_phone` BIGINT NOT NULL COMMENT '用户手机号',
+  `state` TINYINT NOT NULL DEFAULT -1 COMMENT '状态标示:-1 无效 0 成功 1 已付款 2 已发货',
+  `create_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (seckill_id, user_phone) /*联合主键*/
+)ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8 COMMENT='秒杀成功明细表';
