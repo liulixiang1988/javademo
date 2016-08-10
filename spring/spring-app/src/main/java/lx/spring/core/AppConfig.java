630b49360918fd4e6da10fb5d8216f8bd998801e
liulixiang1988 <liulixiang1988@gmail.com>
2016-08-10 16:19:28 +0800

    Spring @ComponentScan, @Component, @Qualifier

--- spring/spring-app/src/main/java/lx/spring/core/AppConfig.java
+++ spring/spring-app/src/main/java/lx/spring/core/AppConfig.java
@@ -2,9 +2,10 @@ package lx.spring.core;
 
 import lx.spring.core.entities.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 import javax.sql.DataSource;
 
@@ -12,25 +13,23 @@ import javax.sql.DataSource;
  * Created by liulixiang on 16/8/9.
  */
 @Configuration
-@Import(InfrastructureConfig.class)
+@ComponentScan(basePackages = "lx.spring.core")
 public class AppConfig {
 
     @Autowired
     private DataSource dataSource;
+
+    @Autowired @Qualifier("redSox")
+    private Team home;
+
+
+    @Autowired @Qualifier("cubs")
+    private Team away;
+
     @Bean
     public Game game() {
-        BaseballGame game = new BaseballGame(redSox(), cubs());
+        BaseballGame game = new BaseballGame(home, away);
         game.setDataSource(dataSource);
         return game;
-    }
-
-    @Bean
-    public Team redSox() {
-        return new RedSox();
-    }
-
-    @Bean
-    public Team cubs() {
-        return new Cubs();
     }
 }
--- spring/spring-app/src/main/java/lx/spring/core/entities/RedSox.java
+++ spring/spring-app/src/main/java/lx/spring/core/entities/RedSox.java
@@ -1,8 +1,11 @@
 package lx.spring.core.entities;
 
+import org.springframework.stereotype.Component;
+
 /**
  * Created by liulixiang on 16/8/9.
  */
+@Component
 public class RedSox implements Team {
     @Override
     public String getName() {
