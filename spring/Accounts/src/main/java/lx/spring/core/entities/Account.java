65c205b9be08a12d3a4b63abc9935c207752fde8
liulixiang1988 <liulixiang1988@gmail.com>
2016-08-23 19:43:52 +0800

    update

---
+++ spring/Accounts/src/main/java/lx/spring/core/entities/Account.java
@@ -0,0 +1,52 @@
+package lx.spring.core.entities;
+
+import java.math.BigDecimal;
+
+public class Account {
+    private Long id;
+    private BigDecimal balance;
+
+    public Account() {
+    }
+
+    public Account(Long id, BigDecimal balance) {
+        this.id = id;
+        this.balance = balance;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", balance=" + balance +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Account account = (Account) o;
+
+        return id != null ? id.equals(account.id) : account.id == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
