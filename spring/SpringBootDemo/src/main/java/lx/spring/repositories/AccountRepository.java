d0396b228e78d3e40b1faacea58d13b2005d867e
liulixiang1988 <liulixiang1988@gmail.com>
2016-09-01 15:54:56 +0800

    spring boot

---
+++ spring/SpringBootDemo/src/main/java/lx/spring/repositories/AccountRepository.java
@@ -0,0 +1,9 @@
+package lx.spring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by liulixiang on 16/9/1.
+ */
+public interface AccountRepository extends JpaRepository<AccountRepository, Long> {
+}
