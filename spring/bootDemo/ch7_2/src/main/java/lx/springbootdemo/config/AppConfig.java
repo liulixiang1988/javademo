a47594a27a949d9f6da00120832e7660dcbc85f8
liulixiang1988 <liulixiang1988@gmail.com>
2016-09-13 21:34:18 +0800

    404 error page

---
+++ spring/bootDemo/ch7_2/src/main/java/lx/springbootdemo/config/AppConfig.java
@@ -0,0 +1,25 @@
+package lx.springbootdemo.config;
+
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.boot.web.servlet.ErrorPage;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by liulixiang on 9/13/16.
+ */
+@Configuration
+public class AppConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
+        factory.setPort(8888);
+        factory.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/404.html"));
+
+        return factory;
+    }
+}
