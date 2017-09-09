1bfc19d3e118dfd0c767b6066a122a652878bd0b
liulixiang1988 <liulixiang1988@gmail.com>
2017-09-09 22:31:31 +0800

    深入实践spring boot初次提交

---
+++ spring/深入实践spring-boot/src/main/java/lx/deepintospringboot/DeepIntoSpringBootApplication.java
@@ -0,0 +1,12 @@
+package lx.deepintospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepIntoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeepIntoSpringBootApplication.class, args);
+	}
+}
---
+++ spring/深入实践spring-boot/src/test/java/lx/deepintospringboot/DeepIntoSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package lx.deepintospringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DeepIntoSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
