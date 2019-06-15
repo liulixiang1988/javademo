c8b88964bd971c37a0b50988e6f1b9dc4fc9d0f6
Liu Lixiang <liulixiang1988@gmail.com>
2019-06-16 06:01:05 +0800

    -XX:MaxPermSize=6M -XX:PermSize=6M永久代在Java6之后被取消了

--- java-interview/src/io/liulx/interview/jvm/model/PermGenErrTest.java
+++ java-interview/src/io/liulx/interview/jvm/model/PermGenErrTest.java
@@ -1,7 +1,23 @@
 package io.liulx.interview.jvm.model;
 
+import java.util.Random;
+
 public class PermGenErrTest {
     public static void main(String[] args) {
+        for (int i = 0; i < 1000; i++) {
+            genRandomString(1000000).intern();
+        }
+        System.out.printf("Mission Complete!!!");
+    }
 
+    private static String genRandomString(int length) {
+        String str = "abcdefghijklmnopqistuvwxyzABCDEFGHIJKLMNOPQISTUVWXYZ0123456789";
+        Random random = new Random();
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < length; i++) {
+            int nextIndex = random.nextInt(62);
+            sb.append(str.charAt(nextIndex));
+        }
+        return sb.toString();
     }
 }
