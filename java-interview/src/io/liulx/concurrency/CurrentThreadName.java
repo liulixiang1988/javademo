19b091ff5c763d060b776b53a460d5bf5ce1afc6
Liu Lixiang <liulixiang1988@gmail.com>
2019-08-02 08:39:19 +0800

    线程名称

---
+++ java-interview/src/io/liulx/concurrency/CurrentThreadName.java
@@ -0,0 +1,7 @@
+package io.liulx.concurrency;
+
+public class CurrentThreadName {
+    public static void main(String[] args) {
+        System.out.println("Current Thread: " + Thread.currentThread().getName());
+    }
+}
