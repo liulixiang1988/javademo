ca902de0caad38fc48a8d4756b8505c8d691d1fe
Liu Lixiang <liulixiang1988@gmail.com>
2019-08-14 21:49:03 +0800

    interrupt

---
+++ java-interview/src/io/liulx/concurrency/InterruptDemo.java
@@ -0,0 +1,31 @@
+package io.liulx.concurrency;
+
+public class InterruptDemo {
+    public static void main(String[] args) throws InterruptedException {
+        Runnable interruptTask = () -> {
+            int i = 0;
+            try {
+                while (!Thread.currentThread().isInterrupted()) {
+                    Thread.sleep(100);
+                    i++;
+                    System.out.println(Thread.currentThread().getName() + " (" + Thread.currentThread().getState() + "). i=" + i);
+                }
+            } catch (InterruptedException e) {
+                System.out.println(Thread.currentThread().getName() + " (" + Thread.currentThread().getState() + ") catch exception " + e);
+            }
+        };
+
+        Thread t1 = new Thread(interruptTask, "t1");
+        System.out.println(t1.getName() + " (" + t1.getState() + ") is new");
+
+        t1.start();
+        System.out.println(t1.getName() + " (" + t1.getState() + ") is started");
+
+        Thread.sleep(300);
+        t1.interrupt();
+        System.out.println(t1.getName() + " (" + t1.getState() + ") is interrupted");
+
+        Thread.sleep(300);
+        System.out.println(t1.getName() + " (" + t1.getState() + ") is terminated");
+    }
+}
