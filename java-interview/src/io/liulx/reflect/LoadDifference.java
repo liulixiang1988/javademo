d407e0ee8bf2e6031c1685ef19daa40207290746
liulixiang1988 <liulixiang1988@gmail.com>
2019-08-22 19:07:30 +0800

    java syntax review

--- java-interview/src/io/liulx/reflect/LoadDifference.java
+++ java-interview/src/io/liulx/reflect/LoadDifference.java
@@ -1,8 +1,20 @@
 package io.liulx.reflect;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 public class LoadDifference {
     public static void main(String[] args) throws ClassNotFoundException {
         //ClassLoader cl = Robot.class.getClassLoader();
         Class r = Class.forName("io.liulx.reflect.Robot");
+        Method[] methods = r.getMethods();
+        for (Method m : methods) {
+            System.out.println(m.getDeclaringClass());
+        }
+
+        Field[] fields = r.getFields();
+        for (Field f : fields) {
+            System.out.println(f.getDeclaringClass());
+        }
     }
 }
