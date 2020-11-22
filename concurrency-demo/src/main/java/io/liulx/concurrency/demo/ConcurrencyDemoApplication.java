f8f5ad6fb86cf8923a82c266467c1d9b9f3fea7f
liulixiang1988 <liulixiang1988@gmail.com>
2020-11-22 16:46:26 +0800

    项目初始化

---
+++ concurrency-demo/src/main/java/io/liulx/concurrency/demo/ConcurrencyDemoApplication.java
@@ -0,0 +1,13 @@
+package io.liulx.concurrency.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ConcurrencyDemoApplication.class, args);
+  }
+
+}
---
+++ concurrency-demo/src/test/java/io/liulx/concurrency/demo/ConcurrencyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.liulx.concurrency.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencyDemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
